eba19afed94a740fcfefbd77db8c53c8e33ff604
Conal McCrory <conalmccrory@gmail.com>
2024-02-26 14:17:45 +0000

    Add secret Key

--- exploreestepona/settings.py
+++ exploreestepona/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7ys*w_$lg0ql_#n57s_oqyrdob+_cjbrsuhe1mqbd0bo2m(6u7'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
