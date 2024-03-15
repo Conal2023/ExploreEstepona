b4af4285f0d9949cb80a886ffe6381f60174e0b3
Conal McCrory <conalmccrory@gmail.com>
2024-03-15 21:20:49 +0000

    Removed placeholder image

--- exploreestepona/settings.py
+++ exploreestepona/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-conal2023-exploreestepo-z7oxqfaj24w.ws-eu110.gitpod.io',
